0a7ba6aff112e51e9a28a647218ce90229bbc61c
Meerab2011 <shakeelpcc@gmail.com>
2025-01-08 15:40:22 +0500

    A7.py

---
+++ Introduction to turtle/A6.py
@@ -0,0 +1,44 @@
+import turtle
+sc=turtle.Screen
+sc.bgcolor("green")
+sc.setup(400,300)
+turtle.title("Welcome to Turtle")
+T=turtle.Turtle()
+#Square
+for i in range(4):
+    T.forward(100)
+    T.left(360/4)
+#triangle
+for i in range(3):
+    T.forward(100)
+    T.left(360/3)
+#octagon
+for i in range(8):
+    T.forward(100)
+    T.left(360/8)
+#circle
+for i in range(360):
+    T.forward(100)
+    T.left(360/360)
+
+T. forward(100)
+T.left(120)
+
+T. forward(100)
+T.left(120)
+
+T.forward (100)
+T.penup()
+T.left(150)
+
+T.forward(50)
+T.right(90)
+T.pendown()
+T.pencolor("red")
+T. forward(100)
+T.left(120)
+
+T. forward(100)
+T.left(120)
+
+T.forward(100)
